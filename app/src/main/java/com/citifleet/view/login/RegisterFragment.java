c6b32082d84cb3c61d3dffeaf6bdf8b4239cb1a1
Viktoria Tsyrulnyk <tsyrulnyk@steelkiwi.com>
2016-03-01 15:04:28 +0200

    Add signup logic

--- app/src/main/java/com/citifleet/view/login/RegisterFragment.java
+++ app/src/main/java/com/citifleet/view/login/RegisterFragment.java
@@ -1,43 +1,74 @@
 package com.citifleet.view.login;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.Size;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.telephony.PhoneNumberUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.citifleet.CitiFleetApp;
 import com.citifleet.R;
+import com.citifleet.util.PrefUtil;
+import com.mobsandgeeks.saripaar.QuickRule;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
 
 import butterknife.Bind;
+import butterknife.BindString;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class RegisterFragment extends Fragment {
+public class RegisterFragment extends Fragment implements Validator.ValidationListener, RegistrationPresenter.RegistrationView {
+    private Validator             validator;
+    private RegistrationPresenter presenter;
+    @NotEmpty
     @Bind(R.id.fullNameEt)
-    EditText fullNameEt;
+    EditText    fullNameEt;
+    @NotEmpty
     @Bind(R.id.usernameEt)
-    EditText usernameEt;
+    EditText    usernameEt;
     @Bind(R.id.phoneEt)
-    EditText phoneEt;
+    EditText    phoneEt;
+    @NotEmpty
+    @Size(min = 6, max = 6)
     @Bind(R.id.hackLicenseEt)
-    EditText hackLicenseEt;
+    EditText    hackLicenseEt;
+    @Email
     @Bind(R.id.emailEt)
-    EditText emailEt;
+    EditText    emailEt;
+    @Password(min = 8, scheme = Password.Scheme.ALPHA_NUMERIC, messageResId = R.string.password_validation_mes)
     @Bind(R.id.passwordEt)
-    EditText passwordEt;
+    EditText    passwordEt;
+    @NotEmpty
+    @ConfirmPassword(messageResId = R.string.password_match_mes)
     @Bind(R.id.confirmPasswordEt)
-    EditText confirmPasswordEt;
+    EditText    confirmPasswordEt;
     @Bind(R.id.signupBtn)
-    Button   signupBtn;
+    Button      signupBtn;
     @Bind(R.id.toolbar)
-    Toolbar  toolbar;
+    Toolbar     toolbar;
     @Bind(R.id.title)
-    TextView title;
+    TextView    title;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindString(R.string.default_error_mes)
+    String      defaultErrorMes;
 
     @Nullable
     @Override
@@ -45,6 +76,10 @@ public class RegisterFragment extends Fragment {
         View view = inflater.inflate(R.layout.signup_fragment, container, false);
         ButterKnife.bind(this, view);
         title.setText(R.string.register);
+        presenter = new RegistrationPresenter(CitiFleetApp.getInstance().getNetworkManager(), this);
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+        validator.put(phoneEt, phoneRule);
         return view;
     }
 
@@ -56,11 +91,79 @@ public class RegisterFragment extends Fragment {
 
     @OnClick(R.id.signupBtn)
     void onSignupBtnClick() {
-
+        validator.validate();
     }
 
     @OnClick(R.id.backBtn)
     void onBackBtnClick() {
         getActivity().onBackPressed();
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        presenter.signup(fullNameEt.getText().toString(), usernameEt.getText().toString(), phoneEt.getText().toString(), hackLicenseEt.getText().toString(),
+                emailEt.getText().toString(), passwordEt.getText().toString(), confirmPasswordEt.getText().toString());
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(getActivity());
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    QuickRule<EditText> phoneRule = new QuickRule<EditText>(0) {
+
+        @Override
+        public boolean isValid(EditText editText) {
+            String phone = editText.getText().toString();
+            return PhoneNumberUtils.isGlobalPhoneNumber(phone); //TODO change to regex
+        }
+
+        @Override
+        public String getMessage(Context context) {
+            return context.getString(R.string.phone_validation);
+        }
+
+    };
+
+    @Override
+    public void onSignUpSuccess(String token) {
+        PrefUtil.setToken(getContext(), token);
+        Toast.makeText(getContext(), "Successful sign up", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void startLoading() {
+        signupBtn.setEnabled(false);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void stopLoading() {
+        signupBtn.setEnabled(true);
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onSignUpFailure(String error) {
+        if (getActivity() != null) {
+            if (error == null) {
+                error = defaultErrorMes;
+            }
+            Toast.makeText(getActivity(), error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onNetworkError() {
+        signupBtn.setEnabled(true);
+        Toast.makeText(getActivity(), getString(R.string.networkMesMoInternet), Toast.LENGTH_LONG).show();
     }
 }
