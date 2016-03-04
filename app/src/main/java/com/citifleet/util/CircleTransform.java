c293fd99e9caa2eeb27c119e16948585641aa2e2
Viktoria Tsyrulnyk <tsyrulnyk@steelkiwi.com>
2016-03-04 16:51:32 +0200

    Add header for dashboard - circle image with frame and blur image in background

---
+++ app/src/main/java/com/citifleet/util/CircleTransform.java
@@ -0,0 +1,60 @@
+package com.citifleet.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import com.squareup.picasso.Transformation;
+
+public class CircleTransform implements Transformation {
+    private int frameSize;
+
+    public CircleTransform(int frameSize) {
+        this.frameSize = frameSize;
+    }
+
+    @Override
+    public Bitmap transform(Bitmap source) {
+        int size = Math.min(source.getWidth(), source.getHeight());
+
+        int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() - size) / 2;
+
+        Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+        if (squaredBitmap != source) {
+            source.recycle();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+        Canvas canvas = new Canvas(bitmap);
+
+
+        float bigRadius = size / 2f;
+        float smallRadius = size / 2f - 2 * frameSize;
+
+
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setAlpha(63);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(frameSize);
+        canvas.drawCircle(bigRadius, bigRadius, smallRadius+frameSize/2, paint);
+
+        paint = new Paint();
+        BitmapShader shader = new BitmapShader(squaredBitmap, BitmapShader.TileMode.CLAMP, BitmapShader.TileMode.CLAMP);
+        paint.setShader(shader);
+        paint.setAntiAlias(true);
+        canvas.drawCircle(bigRadius, bigRadius, smallRadius, paint);
+        squaredBitmap.recycle();
+        return bitmap;
+    }
+
+    @Override
+    public String key() {
+        return "circle";
+    }
+}
--- app/src/main/java/com/citifleet/view/main/DashboardFragment.java
+++ app/src/main/java/com/citifleet/view/main/DashboardFragment.java
@@ -3,22 +3,40 @@ package com.citifleet.view.main;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.citifleet.R;
+import com.citifleet.util.CircleTransform;
 import com.citifleet.view.BaseActivity;
+import com.squareup.picasso.Picasso;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import jp.wasabeef.picasso.transformations.BlurTransformation;
 
 public class DashboardFragment extends Fragment {
+    @Bind(R.id.profileImage)
+    ImageView profileImage;
+    @Bind(R.id.bigProfileImage)
+    ImageView bigProfileImage;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dashboard_fragment, container, false);
         ButterKnife.bind(this, view);
+        int frameSize = getResources().getDimensionPixelSize(R.dimen.profile_image_frame);
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+        TypedValue outValue = new TypedValue();
+        getResources().getValue(R.dimen.profile_image_blur_radius_percent, outValue, true);
+        int blurradius = (int) (screenWidth * outValue.getFloat());
+        Picasso.with(getActivity()).load(R.drawable.testprofile).transform(new CircleTransform(frameSize)).into(profileImage);
+        Picasso.with(getActivity()).load(R.drawable.testprofile).transform(new BlurTransformation(getContext(), blurradius)).into(bigProfileImage);
         return view;
     }
 
