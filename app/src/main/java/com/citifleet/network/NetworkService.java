ccbdcbe4690ca26d31f88c023e32c58e7c02ade4
Viktoria Tsyrulnyk <tsyrulnyk@steelkiwi.com>
2016-03-04 17:20:40 +0200

    Upload user image to server

--- app/src/main/java/com/citifleet/network/NetworkService.java
+++ app/src/main/java/com/citifleet/network/NetworkService.java
@@ -2,10 +2,14 @@ package com.citifleet.network;
 
 import com.citifleet.model.LoginInfo;
 
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
 
 public interface NetworkService {
     @FormUrlEncoded
@@ -22,4 +26,8 @@ public interface NetworkService {
     @FormUrlEncoded
     @POST("reports/")
     Call<Object> report(@Field("report_type") int reportType, @Field("lat") double lat, @Field("lng") double lng);
+
+    @Multipart
+    @PUT("usersupload-avatar/")
+    Call<Object> uploadAvatar(@Part("avatar\"; filename=\"image.png\" ") RequestBody file, @Part("description") RequestBody description);
 }
--- app/src/main/java/com/citifleet/view/main/DashboardFragment.java
+++ app/src/main/java/com/citifleet/view/main/DashboardFragment.java
@@ -1,23 +1,38 @@
 package com.citifleet.view.main;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.citifleet.CitiFleetApp;
 import com.citifleet.R;
 import com.citifleet.util.CircleTransform;
 import com.citifleet.view.BaseActivity;
 import com.squareup.picasso.Picasso;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import jp.wasabeef.picasso.transformations.BlurTransformation;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class DashboardFragment extends Fragment {
     @Bind(R.id.profileImage)
@@ -44,6 +59,51 @@ public class DashboardFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         ButterKnife.unbind(this);
+    }
+
+    @OnClick(R.id.cameraBtn)
+    void onCameraBtnClick() {
+        String descriptionString = "hello, this is description speaking";
+        RequestBody description = RequestBody.create(MediaType.parse("multipart/form-data"), descriptionString);
+
+        File f = new File(getContext().getCacheDir(), "avatar");
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.splash)).getBitmap();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos);
+        byte[] bitmapdata = bos.toByteArray();
+
+//write the bytes in file
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(f);
+            fos.write(bitmapdata);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"), f);
+
+        Call<Object> call = CitiFleetApp.getInstance().getNetworkManager().getNetworkClient().uploadAvatar(requestBody, description);
+        call.enqueue(new Callback<Object>() {
+            @Override
+            public void onResponse(Call<Object> call, Response<Object> response) {
+                Log.v("Upload", "success");
+            }
+
+            @Override
+            public void onFailure(Call<Object> call, Throwable t) {
+                Log.e("Upload", t.getMessage());
+            }
+        });
     }
 
     @OnClick(R.id.homeBtn)
