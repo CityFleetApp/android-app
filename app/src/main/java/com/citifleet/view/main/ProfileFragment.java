e632b4aad48841e9e07cfeee8aeb6a6e9a08ebbb
Viktoria Tsyrulnyk <tsyrulnyk@steelkiwi.com>
2016-03-12 12:07:05 +0200

    Add layout for profile

--- app/src/main/java/com/citifleet/view/main/ProfileFragment.java
+++ app/src/main/java/com/citifleet/view/main/ProfileFragment.java
@@ -6,16 +6,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.citifleet.CitiFleetApp;
 import com.citifleet.R;
 
-import butterknife.ButterKnife;
+import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import android.support.v7.widget.RecyclerView;
 /**
  * Created by vika on 11.03.16.
  */
 public class ProfileFragment extends Fragment {
+    @Bind(R.id.profileImage)
+    ImageView profileImage;
+    @Bind(R.id.bigProfileImage)
+    ImageView bigProfileImage;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.profileFullName)
+    TextView fullName;
+    @Bind({R.id.star1, R.id.star2, R.id.star3, R.id.star4, R.id.star5})
+    List<ImageView> stars;
+    @Bind(R.id.ratingText)
+    TextView ratingText;
+    @Bind(R.id.bio)
+    TextView bio;
+    @Bind(R.id.drives)
+    TextView drives;
+    @Bind(R.id.documents)
+    TextView documents;
+    @Bind(R.id.jobsCompleted)
+    TextView jobsCompleted;
+    @Bind(R.id.imagesList)
+    RecyclerView imagesList;
 
     @Nullable
     @Override
